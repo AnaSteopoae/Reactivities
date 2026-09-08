352917e5334b8d7200a2a663a257818debc5a43b
Anamaria Steopoae <steopoae.anamaria@assim.mapn>
2026-09-08 13:59:22 +0300

    End of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController(Persistence.AppDbContext context) : BaseApiController
+    {
+        // private readonly AppDbContext context;
+        // public ActivitiesController(AppDbContext context)
+        // {
+        //     this.context = context;
+        // }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Domain.Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Domain.Activity>> GetActivityDetail(string id)
+        {
+            var activity = await context.Activities.FindAsync(id);
+            if(activity == null) return NotFound();
+            return activity;
+        }
+    }
+}
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Domain.Activity> Activities { get; set; }
+    }
+}
